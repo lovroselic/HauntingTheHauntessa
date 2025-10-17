759ed87df9905fdb4afc975e1947922077697014
Lovro Selic <lovro.selic@gmail.com>
2025-10-17 18:11:59 +0200

    0.29.6

--- Games/HauntingTheHauntessa/HauntingTheHauntessa.js
+++ Games/HauntingTheHauntessa/HauntingTheHauntessa.js
@@ -156,7 +156,7 @@ const DEBUG = {
 
         console.info("DEBUG::Starting from checkpoint, this may clash with LOAD");
 
-        GAME.level = 83; //56
+        GAME.level = 87; //56
         GAME.gold = 50035;
         GAME.lives = 3;
 
@@ -331,7 +331,7 @@ const INI = {
 };
 
 const PRG = {
-    VERSION: "0.29.5",
+    VERSION: "0.29.6",
     NAME: "Haunting The Hauntessa",
     YEAR: "2025",
     SG: "HTH",
